3478969dba26a774c5920830c297ecc48aa92dc8
Nedim Can Ulusoy <nedimcanulusoy@gmail.com>
2020-06-01 15:20:50 +0300

    Add +1 for range and output text

--- random_number_generator.py
+++ random_number_generator.py
@@ -5,8 +5,8 @@ num_range=[]
 x=int(input('Type for first range: '))
 y=int(input('Type for second range: '))
 
-for nums in range(x,y):
+for nums in range(x,y+1):
     num_range.append(nums)
 
 for rand_num in random.sample(num_range,1):
-    print(rand_num)
+    print('Your random number is: ',rand_num)
